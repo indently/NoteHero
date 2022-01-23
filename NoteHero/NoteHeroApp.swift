c3fa95218a54c7e88fde1211177eb9f2b2f2f488
Code Palace <codepalace@fastmail.se>
2022-01-23 12:02:40 +0100

    Initial Commit

---
+++ NoteHero/NoteHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteHeroApp.swift
+//  NoteHero
+//
+//  Created by Federico on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct NoteHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
